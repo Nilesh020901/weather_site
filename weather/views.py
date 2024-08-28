5d17c530f43098124e72f23b572bd0fff0184699
Nilesh020901 <nileshsoni0209@gmail.com>
2024-08-28 13:13:30 +0530

    working on break for lunch in office

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+# import json to load json data to python dictionary 
+import json
+# urllib.request to make a request to api 
+import urllib.request
 
 # Create your views here.
+
+def index(request):
+    if request.method=="POST":
+        city=request.POST['city']
+        # source contain JSON data from API 
+        source=urllib.request.urlopen
