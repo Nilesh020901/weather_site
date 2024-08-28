24bbf2fe7e51db82bca9adc1fa10abfad88cf230
Nilesh020901 <nileshsoni0209@gmail.com>
2024-08-28 13:55:56 +0530

    after lunch

--- weather/views.py
+++ weather/views.py
@@ -10,4 +10,21 @@ def index(request):
     if request.method=="POST":
         city=request.POST['city']
         # source contain JSON data from API 
-        source=urllib.request.urlopen
+        source=urllib.request.urlopen(
+            'http://api.openweathermap.org/data/2.5/weather?q =' + city + '&appid = your_api_key_here').read()
+        )
+        # converting JSON data to a dictionary 
+        list_of_data=json.loads(source)
+
+        # data for variable list_of_data
+        data={
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + '' + str(list_of_data['coord']['lat']),
+            "temp": str(list_of_data['weather']['temp']) + 'k',
+            "pressure": str(list_of_data['weather']['pressure']),
+            "humidity": str(list_of_data['weather']['humidity']),
+        }
+        print(data)
+        else:
+            data={}
+        return render(request, "weather/index.html", data)
